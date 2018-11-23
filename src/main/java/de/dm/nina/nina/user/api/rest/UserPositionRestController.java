da8c4a9d6287324faed0da9b8c0e4e244989bc05
Helge Chrostek <chr.helge@gmx.de>
2018-11-24 00:24:44 +0100

    Added functionality to UserPositionRestController.

--- src/main/java/de/dm/nina/nina/user/api/rest/UserPositionRestController.java
+++ src/main/java/de/dm/nina/nina/user/api/rest/UserPositionRestController.java
@@ -1,6 +1,12 @@
 package de.dm.nina.nina.user.api.rest;
 
+import de.dm.nina.nina.user.persistence.UserPosition;
+import de.dm.nina.nina.user.persistence.UserPositionRepository;
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,13 +19,24 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @Api
 public class UserPositionRestController {
-    @RequestMapping(method= RequestMethod.GET, value = "/findPerson", produces = APPLICATION_JSON_VALUE)
-    public UserPositionRestDto findPerson(@RequestParam String name){
-        return null;
+    private static final Logger LOG = LoggerFactory.getLogger(UserPositionRestController.class);
+
+    private final UserPositionRepository userPositionRepository;
+
+    @Autowired
+    public UserPositionRestController(UserPositionRepository userPositionRepository){
+        this.userPositionRepository = userPositionRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/findPerson", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserPosition> findPerson(@RequestParam String name) {
+        LOG.info("Tried to find person with name: {}", name);
+        return ResponseEntity.ok(userPositionRepository.getOne(name));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/findPersons", produces = APPLICATION_JSON_VALUE)
-    public List<UserPositionRestDto> findPersons(){
-        return null;
+    public ResponseEntity<List<UserPosition>> findPersons(){
+        LOG.info("Tried to find all persons.");
+        return ResponseEntity.ok(userPositionRepository.findAll());
     }
 }
