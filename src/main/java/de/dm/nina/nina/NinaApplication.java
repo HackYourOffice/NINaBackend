27c841f2bee0ac9ff40d172ee911c1104fd84e18
Helge Chrostek <chr.helge@gmx.de>
2018-11-23 18:58:23 +0100

    Added some application cofiguration

--- src/main/java/de/dm/nina/nina/NinaApplication.java
+++ src/main/java/de/dm/nina/nina/NinaApplication.java
@@ -2,8 +2,12 @@ package de.dm.nina.nina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan
 @SpringBootApplication
+@EnableJpaRepositories
 public class NinaApplication {
 
     public static void main(String[] args) {
