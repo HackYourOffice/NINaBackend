d82127f282a80075dec77a76e56efa0c90f0d4f7
Helge Chrostek <chr.helge@gmx.de>
2018-11-23 18:53:48 +0100

    Renamed User to UserPosition

--- src/main/java/de/dm/nina/nina/user/api/rest/UserPositionRestController.java
+++ src/main/java/de/dm/nina/nina/user/api/rest/UserPositionRestController.java
@@ -1,8 +1,9 @@
-package de.dm.nina.nina.user;
+package de.dm.nina.nina.user.api.rest;
 
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,14 +12,14 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @Api
-public class UserRestController {
+public class UserPositionRestController {
     @RequestMapping(method= RequestMethod.GET, value = "/findPerson", produces = APPLICATION_JSON_VALUE)
-    public UserRestDto findPerson(){
+    public UserPositionRestDto findPerson(@RequestParam String name){
         return null;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/findPersons", produces = APPLICATION_JSON_VALUE)
-    public List<UserRestDto> findPersons(){
+    public List<UserPositionRestDto> findPersons(){
         return null;
     }
 }
