0ed35a0338e504ec0f1566fa661e89efbfc63a70
Helge Chrostek <chr.helge@gmx.de>
2018-11-24 02:32:26 +0100

    Removed rest endpoint, that is now located in correct rest conrtoller

--- src/main/java/de/dm/nina/nina/beacon/api/rest/BeaconRestController.java
+++ src/main/java/de/dm/nina/nina/beacon/api/rest/BeaconRestController.java
@@ -2,8 +2,6 @@ package de.dm.nina.nina.beacon.api.rest;
 
 import de.dm.nina.nina.beacon.persistence.Beacon;
 import de.dm.nina.nina.beacon.persistence.BeaconRepository;
-import de.dm.nina.nina.user.persistence.UserPosition;
-import de.dm.nina.nina.user.persistence.UserPositionRepository;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,25 +18,14 @@ import java.util.List;
 public class BeaconRestController {
     private static final Logger LOG = LoggerFactory.getLogger(BeaconRestController.class);
     private final BeaconRepository beaconRepository;
-    private final UserPositionRepository userPositionRepository;
 
     @Autowired
-    public BeaconRestController(BeaconRepository beaconRepository, UserPositionRepository userPositionRepository) {
+    public BeaconRestController(BeaconRepository beaconRepository) {
         this.beaconRepository = beaconRepository;
-        this.userPositionRepository = userPositionRepository;
-    }
-
-    @RequestMapping(method= RequestMethod.PUT, value = "/publishPosition")
-    public void publishPosition(int id, String person){
-        LOG.info("Tried to publish new Position: id={}; name={}", id, person);
-        Beacon beacon = beaconRepository.getOne(id);
-        Date timestamp = Date.from(Instant.now());
-
-        userPositionRepository.save(new UserPosition(person, beacon, timestamp));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/findBeacons")
-    public ResponseEntity<List<Beacon>> findBeacons(){
+    public ResponseEntity<List<Beacon>> findBeacons() {
         return ResponseEntity.ok(beaconRepository.findAll());
     }
 }
